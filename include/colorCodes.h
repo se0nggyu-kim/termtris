19288e67094bcc3b4f09c243a4a8cdf07ea67730
nekodev <seonggyu@kakao.com>
2020-10-02 14:24:33 +0000

    Add color code as string with enum

---
+++ include/colorCodes.h
@@ -0,0 +1,14 @@
+#ifndef COLOR_CODES_H
+#define COLOR_CODES_H
+
+#define RESET_COLOR "\e[0m"
+#define BLK_BLOCK "\e[40;30m"
+#define RED_BLOCK "\e[41;31m"
+#define GRN_BLOCK "\e[42;32m"
+#define YEL_BLOCK "\e[43;33m"
+#define BLU_BLOCK "\e[44;34m"
+#define MAG_BLOCK "\e[45;35m"
+#define CYN_BLOCK "\e[46;36m"
+#define WHT_BLOCK "\e[47;37m"
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,35 @@
 #include <stdio.h>
+#include <string>
+
+std::string blockColors[8] = {"\e[40;30m  \e[0m", 
+				"\e[41;31m  \e[0m",
+				"\e[42;32m  \e[0m", 
+				"\e[43;33m  \e[0m",
+				"\e[44;34m  \e[0m", 
+				"\e[45;35m  \e[0m",
+				"\e[46;36m  \e[0m", 
+				"\e[47;37m  \e[0m"};
+
+enum class Color : int {
+    BLACK,
+    RED,
+    GREEN,
+    YELLOW,
+    BLUE,
+    PURPLE,
+    CYAN,
+    WHITE
+};
+    
+
 
 int main(void) {
-    printf("start coding!!\n");
+    Color color;
+    printf("\33[10C");	// move cursor to right 10 spaces
+    printf("\33[5B");	// move cursor to bottom 5 spaces
+    printf("%s", blockColors[(int)Color::RED].c_str()); // change background and font color to RED and then reset it
+    
 
+    //printf("\33[u");	// set cursor at 0.0
     return 0;
 }
